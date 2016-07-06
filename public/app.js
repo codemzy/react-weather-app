cbcea24419d5e42e2e7b876de808833f74134b0b
Emma J <codemzy@gmail.com>
2016-07-06 16:25:38 +0000

    Completed lecture 9

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ class Greetr extends React.Component {
         return (
             <div>
                 <h1>Hello React!</h1>
+                <p>This is from the component.</p>
             </div>
         );
     }
