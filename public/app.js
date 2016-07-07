c0fb7f99fc5ed978972dc5f773c8257c710943e0
Emma J <codemzy@gmail.com>
2016-07-07 11:20:41 +0000

    Passed name prop to Greetr in app.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,13 @@
 class Greetr extends React.Component {
     render() {
+        var name = this.props.name;
         return (
             <div>
-                <h1>Hello React!</h1>
+                <h1>Hello {name}!</h1>
                 <p>This is from the component.</p>
             </div>
         );
     }
 };
 
-ReactDOM.render(<Greetr />, document.getElementById('app'));
+ReactDOM.render(<Greetr name="codemzy" />, document.getElementById('app'));
