56ec821c6bbb184367c2e6d9b942fb8685225a18
Emma J <codemzy@gmail.com>
2016-07-07 11:29:48 +0000

    Passed in a message to Greetr in app.js

--- public/app.js
+++ public/app.js
@@ -1,17 +1,21 @@
 class Greetr extends React.Component {
     render() {
         var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
                 <h1>Hello {name}!</h1>
-                <p>This is from the component.</p>
+                <p>{message}</p>
             </div>
         );
     }
 };
 
 Greetr.defaultProps = {
-    name: "React"
+    name: "React",
+    message: "This is from the component."
 };
 
-ReactDOM.render(<Greetr name="codemzy" />, document.getElementById('app'));
+var name = "Codemzy";
+
+ReactDOM.render(<Greetr name={name} message="This is a new message not the default!" />, document.getElementById('app'));
