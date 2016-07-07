ed61ffb445fe335725718d6132f4ea2c34599214
Emma J <codemzy@gmail.com>
2016-07-07 14:48:30 +0000

    Completed lecture 17 and added message element to form and update function

--- public/app.js
+++ public/app.js
@@ -19,19 +19,28 @@ class GreetrForm extends React.Component {
     _onFormSubmit(e) {
         e.preventDefault();
         var name = this.refs.name.value;
+        var message = this.refs.message.value;
+        var update = {};
         if (name.length > 0) {
             this.refs.name.value = '';
-            // pass name up to the Greetr function
-            this.props.onNewName(name);
+            update.name = name;
         }
+        if (message.length > 0) {
+            this.refs.message.value = '';
+            update.message = message;
+        }
+        // pass the update object to the Greetr function
+        this.props.onNewName(update);
+        
     }
     
     render() {
         return (
             <div>
                 <form onSubmit={this._onFormSubmit.bind(this)}>
-                    <input type="text" ref="name" />
-                    <button>Set Name</button>
+                    <input type="text" placeholder="Your name" ref="name" /><br />
+                    <textarea placeholder="Your message..." ref="message"></textarea><br />
+                    <button>Update</button>
                 </form>
             </div>
         );
@@ -44,20 +53,29 @@ class Greetr extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: this.props.name
+            name: this.props.name,
+            message: this.props.message
         };
     }
     
-    _handleNewName(name) {
-        // set state with name passed up from the form
-        this.setState({
-          name: name
-        });
+    _handleNewName(object) {
+        // set state with name if passed up from the form
+        if (object.name) {
+            this.setState({
+              name: object.name
+            });
+        }
+        // set state with message if passed up from the form
+        if (object.message) {
+            this.setState({
+              message: object.message
+            });
+        } 
     }
     
     render() {
         var name = this.state.name;
-        var message = this.props.message;
+        var message = this.state.message;
         return (
             <div>
                 <GreetrMessage name={name} message={message} />
