1e519229f323400929265ad2c15c6fc64c263a04
Emma J <codemzy@gmail.com>
2016-07-13 16:28:12 +0000

    Updated About.js

--- app/components/About.js
+++ app/components/About.js
@@ -3,8 +3,10 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the about page.</p>
+            <h1 className="text-center">About</h1>
+            <p>The Weather Weasel loves the weather. He enjoys travelling to new locations via the vast wires of the internet, checking what the temperature is and reporting back.</p>
+            <p>How does he get the temperature? He holds his tail up in the air and can tell the temperature instantly!</p>
+            <img src="/img/weatherweaselnavy.png" className="float-center"></img>
         </div>
     ); 
 };
