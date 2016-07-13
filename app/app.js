277fb613d089bbae3c391a5dc467ae8d2af91e7e
Emma J <codemzy@gmail.com>
2016-07-13 15:29:30 +0000

    Updated webpack config and app.js to add foundation and jquery

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 // routes
 var routes = require('./config/routes');
 
