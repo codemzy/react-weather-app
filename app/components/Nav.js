421d8e08dc6de6a6781a38881f8ac800f48e7bbd
Emma J <codemzy@gmail.com>
2016-07-13 15:48:23 +0000

    Switched Nav.js from stateless functional component back to full React component

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,29 +1,31 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-    return (
-        <div className="top-bar">
-          <div className="top-bar-left">
+class Nav extends React.Component {
+    render() {
+        return (
+            <div className="top-bar">
+              <div className="top-bar-left">
+                    <ul className="menu">
+                    <li className="menu-text">Weather Weasel</li>
+                      <li>
+                        <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+                      </li>
+                      <li>
+                        <Link to="/about" activeClassName="active">About</Link>
+                      </li>
+                      <li>
+                        <Link to="/examples" activeClassName="active">Examples</Link>
+                      </li>
+                    </ul>
+              </div>
+              <div className="top-bar-right">
                 <ul className="menu">
-                <li className="menu-text">Weather Weasel</li>
-                  <li>
-                    <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-                  </li>
-                  <li>
-                    <Link to="/about" activeClassName="active">About</Link>
-                  </li>
-                  <li>
-                    <Link to="/examples" activeClassName="active">Examples</Link>
-                  </li>
                 </ul>
-          </div>
-          <div className="top-bar-right">
-            <ul className="menu">
-            </ul>
-          </div>
-        </div>
-    );
-};
+              </div>
+            </div>
+        );
+    }
+}
 
 module.exports = Nav;
