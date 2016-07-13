3eb014ace517a1a9c1a328fd15dad82a238d3673
Emma J <codemzy@gmail.com>
2016-07-13 16:12:00 +0000

    Updated the Examples component

--- app/components/Examples.js
+++ app/components/Examples.js
@@ -1,10 +1,28 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page.</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out.</p>
+            <ol>
+                <li>
+                    <Link to='/?location=London'>London, UK</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Madrid'>Madrid, Spain</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Sydney'>Sydney, Australia</Link>
+                </li>
+                <li>
+                    <Link to='/?location=NewYork'>New York, USA</Link>
+                </li>
+            </ol>
         </div>
     ); 
 };
