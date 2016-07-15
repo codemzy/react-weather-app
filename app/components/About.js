70b67ed0bdcb929aa970e42db7e0947c193e1954
Emma J <codemzy@gmail.com>
2016-07-15 11:08:15 +0000

    Added favicon and updated image

--- app/components/About.js
+++ app/components/About.js
@@ -6,7 +6,7 @@ var About = (props) => {
             <h1 className="text-center page-title">About</h1>
             <p>The Weather Weasel loves the weather. He enjoys travelling to new locations via the vast wires of the internet, checking what the temperature is and reporting back.</p>
             <p>How does he get the temperature? He holds his paw up in the air and can tell the temperature instantly!</p>
-            <img src="/img/weatherweaselnavy.png" className="float-center"></img>
+            <img src="/img/weatherweasel.png" className="float-center"></img>
         </div>
     ); 
 };
