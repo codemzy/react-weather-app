2c08d6a88940350d5f011946bd2ef65a62fd8030
Emma J <codemzy@gmail.com>
2016-07-15 13:40:07 +0000

    Updated About.js with links

--- app/components/About.js
+++ app/components/About.js
@@ -6,7 +6,12 @@ var About = (props) => {
             <h1 className="text-center page-title">About</h1>
             <p>The Weather Weasel loves the weather. He enjoys travelling to new locations via the vast wires of the internet, checking what the temperature is and reporting back.</p>
             <p>How does he get the temperature? He holds his paw up in the air and can tell the temperature instantly!</p>
-            <img src="/img/weatherweasel.png" className="float-center"></img>
+              <div className="text-center">
+                <p>Built by codemzy.</p>
+                <a href="https://www.twitter.com/codemzy" target="_blank"><i className="fa fa-twitter"></i> Twitter</a>&nbsp;&nbsp;&nbsp;
+                <a href="https://github.com/codemzy" target="_blank"><i className="fa fa-github"></i> GitHub</a>&nbsp;&nbsp;&nbsp;
+                <a href="https://codemzy.github.io" target="_blank"><i className="fa fa-th"></i> Portfolio</a>
+              </div>
         </div>
     ); 
 };
